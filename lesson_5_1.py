97cc70e69e41161a24f620cc28b913529462f1ae
Ira1234567 <iraowl1234@.com>
2021-11-08 18:39:36 +0300

    1

--- lesson_5_1.py
+++ lesson_5_1.py
@@ -1,4 +1,9 @@
 import random
 a = [random.randint(1,5) for _ in range(5)]
 b = [random.randint(1,5) for _ in range(6)]
-print(len([i for i in a if i not in b])+len([i for i in b if i not in a]))
+a.extend(b)
+print(len([i for i in a if a.count(i)==1]))
+
+
+
+
