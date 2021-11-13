c5b9b77853673a52596aa0556a672563d3d623c2
Ira1234567 <iraowl1234@.com>
2021-11-13 18:39:43 +0300

    1

--- lesson_6_2.py
+++ lesson_6_2.py
@@ -1,7 +1,9 @@
 a = set([int(i) for i in input()])
 b = set([int(i) for i in input()])
-c = {*a, *b}
-if c == a:
-    print("a является суперсетом для b")
+print(a)
+print(b)
+b.update(a)
+if b == a:
+   print("a является суперсетом для b")
 else:
     print("a не является суперсетом для b")
