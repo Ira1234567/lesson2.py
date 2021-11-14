ee1d465bd8ffe1164ad6666504361f6a8bce2de4
Ira1234567 <iraowl1234@.com>
2021-11-14 22:01:01 +0300

    1

--- lesson_7_2.py
+++ lesson_7_2.py
@@ -2,12 +2,15 @@ def arithmetic(a, b, c):
     a = int(a)
     b = int(b)
     if c == "+":
-        print(a+b)
+        return a+b
     elif c == "-":
-        print(a-b)
+        return a-b
     elif c == "*":
-        print(a*b)
+        return a*b
     elif c == "/":
-        print(a/b)
+        return a/b
     else:
-        print("Неизвестная операция")
+        return "Неизвестная операция"
+
+
+print(arithmetic(5, 6, "+"))
