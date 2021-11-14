5b5a627a92e08fe0d41b0804365b3840ae59234f
Ira1234567 <iraowl1234@.com>
2021-11-14 21:49:37 +0300

    1

--- lesson_6_2.py
+++ lesson_6_2.py
@@ -2,8 +2,7 @@ a = set([int(i) for i in input()])
 b = set([int(i) for i in input()])
 print(a)
 print(b)
-b.update(a)
-if b == a:
+if a >= b:
    print("a является суперсетом для b")
 else:
     print("a не является суперсетом для b")
