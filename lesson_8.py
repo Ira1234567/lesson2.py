a971a67a6ec3d024543702735f3b4ee7fa44dc69
Ira1234567 <iraowl1234@.com>
2021-11-21 14:20:15 +0300

    1

--- lesson_8.py
+++ lesson_8.py
@@ -1,34 +1,35 @@
+def decorator1(funk1):
+    def funk2(check_password, authenticate):
+        funk1
+        def check_password(username, password):
+            usernames_and_passwords = {'name1': '1234', 'name2': '2345', 'name3': '3456', 'name4': '4567'}
+            if username in usernames_and_passwords and usernames_and_passwords[username] == password:
+                return True
+            else:
+                return False
+        def authenticate():
+            return True
+        if check_password(username1, password1) == True and authenticate() == True:
+            return True
+    return funk2
+
+@decorator1
 def login(username, password):
     if len(password) >= 4 and len(username) >= 4:
         return True
     else:
         return False
 
-
-def check_password(username, password):
-    usernames_and_passwords = {'name1': '1234', 'name2': '2345', 'name3': '3456', 'name4': '4567'}
-    if username in usernames_and_passwords and usernames_and_passwords[username] == password:
-        return True
-    else:
-        return False
-
-
-def authenticate(func1, func2):
-    if func1 == True and func2 == True:
-        return True
-    else:
-        return False
-
 a = 3
 while a >=1:
-    username_1 = (str(input("Введите имя пользователя: ")))
-    password_1 = (str(input("Введите пароль: ")))
-    if authenticate(login(username_1, password_1), check_password(username_1, password_1)) == True:
+    username1 = input("Введите имя пользователя: ")
+    password1 = input("Введите пароль: ")
+    if login(username1, password1) == True:
         print("Вы в системе!")
         break
     else:
         if a > 1:
-            print("Не правильное имя или пароль. У вас осталось ", a-1, " попыток")
+            print("Не правильное имя или пароль. У вас осталось ", a - 1, " попыток")
         else:
             print("Попытки истекли!")
     a -= 1
