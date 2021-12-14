b1dc7d04ae64413157641fb93607cc9c2adae1ce
Ira1234567 <iraowl1234@.com>
2021-12-14 19:04:47 +0300

    1

--- lesson_12.py
+++ lesson_12.py
@@ -1,12 +1,22 @@
 
 import json
 
+def decorator1(func1):
+    def func2(username, password):
+      user_data = [username, password]
+      with open('new_file', 'w') as f:
+        json.dump(user_data, f)
+        f.close()
+      return func1(username, password)
+    return func2
+
+@decorator1
 def login(username, password):
     if len(password) >= 4 and len(username) >= 4:
-        user_data = [username, password]
-        with open('new_file', 'w') as f:
-          json.dump(user_data,f)
-        f.close()
+        return username, password
+
+
+
 
 
 login('name1','1234')
